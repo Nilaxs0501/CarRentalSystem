3459f5dfef95cf58e588e3d503187952da2e6585
Nilaxs0501 <nobertnilaxshan@gmail.com>
2025-09-03 14:52:38 +0530

    create

---
+++ CarRentalSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CarRentalSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace CarRentalSystem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+
+        }
+
+        public DbSet<Car> Cars { get; set; }
+
+    }
+}
---
+++ CarRentalSystem/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿namespace CarRentalSystem.Models
+{
+    public class Admin
+    {
+        public Guid Id { get; set; }
+        public  string Name { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
